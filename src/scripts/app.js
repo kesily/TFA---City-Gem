91d4cd218e0249353a4f89bb0c6b1f874702795e
Kesily <kesily.ouattara@gmail.com>
2023-05-26 18:14:42 +0200

    deuxieme comit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
-console.info('Hello world');
+const menuBurger = document.querySelector('.menu__burger');
+const menu = document.querySelector('.menu__list');
+const cross = document.querySelector('.cross');
+
+menuBurger.addEventListener('click', function(){
+    menu.classList.toggle('menu--open');
+});
+
+cross.addEventListener('click', function(){
+    menu.classList.remove('menu--open');
+});
